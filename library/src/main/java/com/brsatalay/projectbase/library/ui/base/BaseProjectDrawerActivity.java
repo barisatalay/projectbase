c62ea53dc6254008c4613ea1714743191c95f448
Barış ATALAY <baris.atalay@santsg.com>
2018-06-25 15:50:50 +0300

    Bug fixed

--- library/src/main/java/com/brsatalay/projectbase/library/ui/base/BaseProjectDrawerActivity.java
+++ library/src/main/java/com/brsatalay/projectbase/library/ui/base/BaseProjectDrawerActivity.java
@@ -7,7 +7,6 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewStub;
@@ -18,13 +17,11 @@ import com.brsatalay.projectbase.library.core.util.UtilsKeyboard;
 
 public abstract class BaseProjectDrawerActivity extends BaseProjectActivity implements DrawerLayout.DrawerListener{
     private DrawerLayout drawerLayout;
-    private boolean isLeftDrawerInfoLoaded;
 
     private NavigationView navigationView;
     private NavigationView navigationView_Right;
 
     private boolean doubleBackToExitPressedOnce = false;
-
 
     @Override
     protected void onCreate(Bundle bundle){
@@ -32,7 +29,6 @@ public abstract class BaseProjectDrawerActivity extends BaseProjectActivity impl
         super.onCreate(bundle);
         setContentView(R.layout.activity_drawer_base);
         initView();
-        this.isLeftDrawerInfoLoaded = false;
 
         closeLeftDrawer();
 
@@ -83,12 +79,12 @@ public abstract class BaseProjectDrawerActivity extends BaseProjectActivity impl
         }
     }
 
-    private void closeLeftDrawer() {
+    public void closeLeftDrawer() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START))
             drawerLayout.closeDrawer(GravityCompat.START);
     }
 
-    protected void closeRightDrawer() {
+    public void closeRightDrawer() {
         if (drawerLayout.isDrawerOpen(GravityCompat.END))
             drawerLayout.closeDrawer(GravityCompat.END);
     }
@@ -166,83 +162,7 @@ public abstract class BaseProjectDrawerActivity extends BaseProjectActivity impl
 
     @Override
     public void onDrawerStateChanged(int newState) {
-        loadLeftDrawerInfo();
     }
-
-    protected void loadLeftDrawerInfo() {
-//        if (isLeftDrawerInfoLoaded) return;
-//
-//        nav_view_logo = findViewById(R.id.nav_view_logo);
-//        nav_view_guide_name = findViewById(R.id.nav_view_guide_name);
-//        leftDrawerList = findViewById(R.id.leftDrawerList);
-//        nav_view_version = findViewById(R.id.nav_view_version);
-//
-//        nav_view_version.setText( getString(R.string.appversion) + " " + UtilsGeneral.getAppVersionName(getApplicationContext()));
-//
-//        findViewById(R.id.nav_view_signout).setOnClickListener(this);
-//        findViewById(R.id.nav_view_close_left).setOnClickListener(this);
-//
-//        loadGuideInfo();
-//
-//        DrawerAdapter adapter = new DrawerAdapter(SejourApplication.getInstance().getDrawerList());
-//        adapter.setListener((view, position) -> {
-//            int stringResource = SejourApplication.getInstance().getDrawerList().get(position).getText();
-//            Intent activityIntent = null;
-//            //ActivityOptions options =
-//            //        ActivityOptions.makeCustomAnimation(getApplicationContext(), R.anim.fade_in, R.anim.fade_out);
-//
-//            UtilsFirebase.addClickAnalytics(this, String.valueOf(view.getId()),"Drawer Item","row");
-//
-//
-//            switch (stringResource){
-//                case R.string.nav_view_home: activityIntent = MainActivity.newInstance(getApplicationContext()); break;
-//                case R.string.nav_view_toursale: activityIntent = TourSaleActivity.newInstance(getApplicationContext()); break;
-//                case R.string.nav_view_summary: activityIntent = SaleReportActivity.newInstance(getApplicationContext()); break;
-////                case R.string.nav_view_infcoctail: activityIntent = DownloadedActivity.newInstance(getApplicationContext()); break;
-//                case R.string.nav_view_tourfacility: activityIntent = TourFacilityActivity.newInstance(getApplicationContext()); break;
-//                case R.string.nav_view_notification: activityIntent = AnnouncementActivity.newInstance(getApplicationContext(), false); break;
-//                case R.string.nav_view_settings: activityIntent = SettingActivity.newInstance(getApplicationContext());  break;
-//            }
-//            if (activityIntent != null) {
-//                startActivity(activityIntent);
-//                //overridePendingTransition(R.anim.activity_slide_from_right, R.anim.activity_slide_to_left);
-//            }
-//
-//        });
-//
-//        getActivity().runOnUiThread(() -> {
-//            UtilsRecyclerView.prepareVerticalManRecycler(leftDrawerList);
-//            leftDrawerList.setAdapter(adapter);
-//        });
-//
-//        isLeftDrawerInfoLoaded = true;
-    }
-
-    private void loadGuideInfo() {
-//        nav_view_guide_name.setText(SejourApplication.getInstance().getSessionManager().getActiveUser().getAdi());
-//
-//        mdlSejourParam sejourParam = new Select().from(mdlSejourParam.class).executeSingle();
-//
-//        if(sejourParam != null && sejourParam.getMobBiletLogo() != null && !sejourParam.getMobBiletLogo().isEmpty()){
-//            String path = UtilsImage.saveBase64(getApplicationContext(), sejourParam.getMobBiletLogo(), "MobBiletLogo.png", false);
-//
-//            UtilsImage.loadImage(SejourApplication.getInstance().getPicasso(),"file://" + path, nav_view_logo, null, ImageDisplayStyle.CACHEONLY);
-//        }
-    }
-
-//    @Override
-//    public void onClick(View view) {
-//        switch (view.getId()){
-//            case R.id.nav_view_signout:
-//                SejourApplication.getInstance().clearUserDataForLogout();
-//                startActivity(LoginActivity.newInstance(getApplicationContext()));
-//                break;
-//
-//            case R.id.nav_view_close_left:
-//                closeLeftDrawer();
-//                break;
-//        }
-//    }
 
     public void loadRightDrawer(int drawerLayoutResId) {
         drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED, findViewById(R.id.nav_view_right) );
